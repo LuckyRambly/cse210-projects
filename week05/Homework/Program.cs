84222034acb59da15fb7045ffdf900056e49bbfc
LuckyRambly <ramonrayopro@gmail.com>
2025-10-10 16:54:38 -0400

    W05LA - Inheritance topic

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -1,9 +1,33 @@
 using System;
 
-class Program
+namespace Homework
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---Assignment test---");
+            Assignment generalAssignment = new Assignment("Samuel Bennett", "Multiplication");
+            Console.WriteLine(generalAssignment.GetSummary()); 
+            Console.WriteLine("\n" + new string('-', 20) + "\n");
+            Console.WriteLine("---Math Assignment test---");
+            MathAssignment math = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+            
+            string mathSummary = math.GetSummary();
+            string homeworkList = math.GetHomeworkList(); 
+            
+            Console.WriteLine(mathSummary);
+            Console.WriteLine(homeworkList);
+            Console.WriteLine("\n" + new string('-', 20) + "\n");
+
+            Console.WriteLine("---Writing Assignment test---");
+            WritingAssignment writing = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+
+            string writingSummary = writing.GetSummary();
+            string writingInfo = writing.GetWritingInformation();
+
+            Console.WriteLine(writingSummary);
+            Console.WriteLine(writingInfo);
+        }
     }
 }

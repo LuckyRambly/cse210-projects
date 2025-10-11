84b605e3731d3749a33a95ffceb2b4dab24c648c
LuckyRambly <ramonrayopro@gmail.com>
2025-10-10 22:46:39 -0400

    W06LA - Poly exercises

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,28 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+public class Program
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+
+        // shapes
+        shapes.Add(new Square("Red", 5));
+        shapes.Add(new Rectangle("Blue", 4, 6));
+        shapes.Add(new Circle("Green", 3.5));
+        shapes.Add(new Square("Yellow", 2));
+                
+        Console.WriteLine("--- Calculating area---");
+
+        foreach (Shape shape in shapes)
+        {
+            double area = shape.GetArea();
+            
+            Console.WriteLine($"Color: {shape.GetColor()}, Type: {shape.GetType().Name}, Area: {area:F2}");
+        }
+        
+        Console.WriteLine("-----------------");
+
     }
 }
